125add553a8641c61582d2d0ee921adb89306350
Krmanrique <krmanriquesanchez@gmail.com>
2026-03-11 17:20:22 -0500

    feat: usersModel is created

---
+++ backend/src/models/usersModel.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Users = sequelize.define('users', {
+
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: { notEmpty: { msg: 'El nombre no puede estar vacío' } },
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: { msg: 'El email ya está registrado' },
+    validate: { isEmail: { msg: 'Email inválido' } },
+  },
+  contrasena_hash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  rol: {
+    type: DataTypes.ENUM('admin', 'profesor', 'estudiante'),
+    allowNull: false,
+    defaultValue: 'estudiante',
+  },
+  fecha_registro: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  }, 
+
+  }, {
+    tableName: 'users',
+    // Excluir contraseña en consultas por defecto
+    defaultScope: {
+      attributes: { exclude: ['contrasena_hash'] },
+    },
+    scopes: {
+      withPassword: { attributes: {} },
+    },
+
+});
+
+module.exports = Users;
